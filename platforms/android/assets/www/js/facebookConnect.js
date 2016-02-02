c11ad199706f9d429ed31677787765f64f25135e
nishchal surface <nishchal.pandey@mavs.uta.edu>
2016-02-02 00:06:00 -0600

    first commit

---
+++ platforms/android/assets/www/js/facebookConnect.js
@@ -0,0 +1,138 @@
+
+/*	This module requires facebook plugin for cordova to be installed
+*	in order for it to function.
+*	This service must be initiated after the device is 
+* 	ready. Use of $ionicPlatform.ready(function(){ }) is
+*	highly recommended.
+*	
+*	Author: Nishchal Pandey
+*	Date: 2016-01-14
+*/
+
+
+var facebook = angular.module('facebookModule',[]);
+facebook.factory('facebookServices',function($ionicPopup, $window, $q, $ionicPlatform, $http){
+	 
+	 facebookLoginDialog = function(){
+					$ionicPopup.show({
+				      title: ' <p>Connect with Facebook  for  better<br> viewing experience</p>',
+				      buttons: [{ 
+				        text: '<img width= 200 id="facebook-connect-button" src="img/facebook-connect-button.png">',
+				        type: 'button-clear',
+				        onTap: function(e){
+				          login();
+				        }
+				      }] 
+				    })
+				}
+
+	simpleAlert = function(content){
+		var simpleDialog = $ionicPopup.show({
+	      title: content
+	    })
+
+		setTimeout(function(){
+			simpleDialog.close();
+		}, 2000);
+
+	}
+				
+ 	test = function(){
+		alert("success");
+	}
+
+	apiRequestWallPost = function () {	
+	    if($window.localStorage.getItem('postPermission') == null){
+	        facebookConnectPlugin.api( "me/?fields=id,email,first_name", ["publish_actions"],
+                  function (response) {
+                        $window.localStorage.setItem("postPermission","set");
+                  },
+                  function (response) {
+                    console.log("error occured");
+                });
+	        }
+	    return false;
+	}
+	
+	apiGetPublicProfile = function () { 
+	    facebookConnectPlugin.api( "me/?fields=id,email,first_name,last_name", ["public_profile"],
+	        function (response) {
+	    		apiRequestWallPost();
+	           simpleAlert("<h4 style='text-align:center'>Welcome back <br>" + response.first_name + "!</h4>");
+	           var responseDb = sendUserInfoToDb(response);
+	    	},
+	        function (response) { 
+	            simpleAlert("something went wrong");
+	    	}); 
+	}	
+	
+	login = function () {
+	    facebookConnectPlugin.login( ["email"],
+	        function (response) {
+	    		apiGetPublicProfile();
+	    	})
+	
+	}
+	
+	getStatus = function () { 
+		facebookConnectPlugin.getLoginStatus( 
+	        function (response) {
+	                    if(response.status == "unknown"){
+	                       $window.localStorage.removeItem('postPermission');
+	                        facebookLoginDialog();
+	                    }else{
+	                    	apiGetPublicProfile();
+	                    }
+	        	},
+	        function (response) {
+	               $window.localStorage.removeItem('postPermission');
+	        		facebookLoginDialog();
+	        	});
+	}
+	
+	sendUserInfoToDb = function(content){
+		var postData = $.param({userInfo: content});
+		$http({
+			url:"http://cinemagharhd.com/php/user_validation.php",
+			data: postData,
+			method:"POST",
+			headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+		});
+	}
+
+	
+	
+	postToFacebook = function (content , link) { 
+	    facebookConnectPlugin.getAccessToken(
+
+	        function (response) { 
+	        		$http({
+	        			url: "http://cinemagharhd.com/php/post_to_facebook.php",
+	        			method: "GET",
+	        			params: {access_token:response, text_content: content, banner_link:link}
+	        		}).then(
+	        			function(success){
+	        				console.log('success : ' + success);
+	        			},
+	        			function(error){
+	        				console.log('error : ' + error);
+	        			}
+	        		)
+	        			
+	        	},
+	        function (response) { 
+	        	simpleAlert("Unable to Post, user is not logged in!");
+	        });
+	}
+
+	document.addEventListener("deviceready", function(){
+		getStatus();
+	}, false);
+
+	return{
+		postToFb: postToFacebook,
+		login: login,
+		test: test 
+	}
+
+});
---
+++ www/js/ratingModule.js
@@ -0,0 +1,84 @@
+var ratingModule = angular.module('ratingModule',[]);
+
+ratingModule.service('ratingService', function($ionicPopup, facebookServices, movieFactory){
+	
+	this.ratingDiag = {
+		show: function(scope, movie){
+			scope.rating ={};
+			scope.rating.value = 0;
+			$ionicPopup.show({
+				title: "How would you rate this movie?",
+				template: 	'<span align="right">'+
+							'<rating-stars rating="rating.value"></rating-stars>'+
+							'</span>',
+				scope: scope,
+				buttons:[{
+					text:"Ok",
+					type:'button-positive',
+					onTap: function(e){
+						var ratingVal = scope.rating.value;
+						
+						var movieTitle = movie.name;
+						var stars = '';
+						var banner_link = movie.banner_name;
+
+						for (i = 1; i <= 5; i++)
+							if(i <= ratingVal)
+								stars = stars + "\u2605";
+							else
+								stars = stars + "\u2606";
+							
+							facebookServices.postToFb("I watched and rated \""+ movieTitle +"\" "+stars+ " on Cinemaghar www.cinemagharhd.com", banner_link);
+							movieFactory.sendRatingToDb(ratingVal, movieTitle);
+					}
+				}]
+			});
+		}
+
+	}
+	
+});
+
+ratingModule.directive('ratingStars',function(){
+	return{
+		restrict: 'E',
+		scope:{
+			rating: '='
+		},
+		compile: function (tElement, tAttrs){
+
+			var getStar = function(i) {
+				var star = angular.element('<img>');
+				star.attr({
+						'src':"img/ratingStars/outline-star-xxl.png",
+						'index': i
+					})
+					.css('width', "40px")
+					
+					return star;
+				}
+			for(i = 1; i <= 5; i ++){
+				tElement.append(getStar(i));				
+			}
+				
+
+			return function(tScope, tElement, tAttrs){
+				
+			 	tElement.on('click',function(e){
+				var index = angular.element(e.target).attr('index');
+				var stars = tElement.children();
+					for (i=0; i < 5; i ++){
+						if(i < index)
+							stars[i].src = "img/ratingStars/Star-Full-icon.png";
+						else
+							stars[i].src = "img/ratingStars/outline-star-xxl.png";
+					}
+					
+					tScope.$apply(function(){
+						tScope.rating = index;
+					});
+				});
+			}
+		}
+	}
+})
